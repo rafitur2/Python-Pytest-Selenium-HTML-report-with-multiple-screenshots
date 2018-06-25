8e882e5005f5cfdb5087f48c5a93cd7fc9ff3fb6
Rafi Diamant <rafi.diamant@amdocs.com>
2018-06-25 13:53:00 +0300

    This is my first commit!

---
+++ Test_Runner.py
@@ -0,0 +1,32 @@
+import sys
+import traceback
+from selenium import webdriver
+import time
+import unittest
+from selenium.webdriver.support.select import Select
+import pytest
+
+class TestBrowser():
+
+    def test_teach(self,browser):
+        Value = "Under 20"
+        browser.get("http://www.teachmeselenium.com/automation-practice/")
+        dropdown = Select(browser.find_element_by_name("age"))
+        dropdown.select_by_value(Value)
+        selected_value=dropdown.first_selected_option
+        browser.save_screenshot("Test1.png")
+        assert selected_value.text == Value
+        assert False
+
+    def test_facebook(self,browser):
+        Value = "Under 20"
+        browser.get("http://www.facebook.com/")
+        # browser.save_screenshot("Test2.png")
+        assert "xyy" in browser.title
+
+    def test_google(self,browser):
+        Value = "Under 20"
+        browser.get("http://www.google.com/")
+        # browser.save_screenshot("Test2.png")
+        assert "xyy" in browser.title
+        assert True
---
+++ conftest.py
@@ -0,0 +1,51 @@
+import pytest
+
+# @pytest.yield_fixture(scope='session')
+# def browser():
+#     from selenium import webdriver
+#     driver = webdriver.Chrome()
+#     yield driver
+#     driver.quit()
+
+from selenium import webdriver
+import pytest
+driver = None
+
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    """
+    Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+    :param item:
+    """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_")+".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:600px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+
+def _capture_screenshot(name):
+    driver.get_screenshot_as_file(name)
+
+
+@pytest.fixture(scope='session', autouse=True)
+def browser():
+    global driver
+    if driver is None:
+        driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+    return driver
+
+
